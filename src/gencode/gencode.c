a3201179f6f6188aaedb4f2edef87b1f2a1ee525
liaotonglang <liaotonglang@gmail.com>
2022-03-25 11:03:59 +0800

    move static function from .h to .c
    they used by gencode.c file only

--- src/gencode/gencode.c
+++ src/gencode/gencode.c
@@ -11,6 +11,50 @@
 
 #define DEPENDENCY_HASH_MAP_BUCKET_SIZE 1280
 
+static unsigned int hash_s(const char* data, size_t n, unsigned int seed) {
+    // unsigned int seed = 0xbc9f1d34;
+    // Similar to murmur hash
+    const unsigned int m = 0xc6a4a793;
+    const unsigned int r = 24;
+    const char* limit = data + n;
+    unsigned int h = seed ^ (n * m);
+
+    // Pick up four bytes at a time
+    while (data + 4 <= limit) {
+        unsigned int w = *(unsigned int*)(data);
+        data += 4;
+        h += w;
+        h *= m;
+        h ^= (h >> 16);
+    }
+
+    // Pick up remaining bytes
+    switch (limit - data) {
+        case 3:
+            h += (unsigned char)(data[2]) << 16;
+            __attribute__((fallthrough));
+        case 2:
+            h += (unsigned char)(data[1]) << 8;
+            __attribute__((fallthrough));
+        case 1:
+            h += (unsigned char)(data[0]);
+            h *= m;
+            h ^= (h >> r);
+            break;
+    }
+    return h;
+}
+
+inline static unsigned int hash_2s(sstr_t key1, sstr_t key2) {
+    unsigned int res = 0xbc9f1d34;
+    sstr_t tmp = sstr_dup(key1);
+    sstr_append_of(tmp, "#", 1);
+    sstr_append(tmp, key2);
+    res = hash_s(sstr_cstr(tmp), sstr_length(tmp), res);
+    sstr_free(tmp);
+    return res;
+}
+
 static void gen_code_struct_marshal_array(struct struct_container* st,
                                           sstr_t source);
 
