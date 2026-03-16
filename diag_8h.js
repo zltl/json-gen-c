cffa09e74d9afdf22fcaf6ca079e30c580e6741f
zltl <zltl@users.noreply.github.com>
2026-03-16 03:38:57 +0000

    deploy: 2e4d65df488f370f75324705cb0e454df50c0ae0

--- diag_8h.js
+++ diag_8h.js
@@ -7,7 +7,7 @@ var diag_8h =
       [ "DIAG_WARNING", "diag_8h.html#a99d5b1c815779e8cef37923a81971ca2a4d0d13f3012b5edaf694ede6f6e0fe1a", null ],
       [ "DIAG_NOTE", "diag_8h.html#a99d5b1c815779e8cef37923a81971ca2a7e63f34cb341ea05222c3d1ea11b7307", null ]
     ] ],
-    [ "diag_emit", "diag_8h.html#acc41c2f0b3ec07426e5aa7c25f9ca824", null ],
+    [ "diag_emit", "diag_8h.html#a4cfb48bb0694e49f926707b8d4aebdca", null ],
     [ "diag_engine_free", "diag_8h.html#a13ba0c92327df716d0aa3c6a5f75e77f", null ],
     [ "diag_engine_new", "diag_8h.html#a984de7af0bfffded4fead8918fa9e8e6", null ],
     [ "diag_error_count", "diag_8h.html#a3548bd22f9ab99dacaefbe95d6cd9a6a", null ],
